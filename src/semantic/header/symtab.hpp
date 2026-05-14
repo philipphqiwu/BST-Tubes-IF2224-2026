30110554af9776a8adfb0ae1262fe39c82484a35
Emilio Justin <emilio.justin54@gmail.com>
2026-05-14 17:10:37 +0700

    feat(semantic): symbol table header declaration

---
+++ src/semantic/header/symtab.hpp
@@ -0,0 +1,71 @@
+#ifndef SYMTAB_HPP
+#define SYMTAB_HPP
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+enum class ObjClass {
+    UNKNOWN = 0,
+    CONSTANT,
+    VARIABLE,
+    TYPE,
+    PROCEDURE,
+    FUNCTION,
+    PROGRAM,
+    FIELD 
+};
+
+struct TabEntry {
+    std::string name;
+    ObjClass obj;
+    int type;
+    int ref;
+    int nrm;
+    int lev;
+    int adr;
+    int link;
+};
+
+struct BTabEntry {
+    int last;
+    int lpar;
+    int psze;
+    int vsze;
+};
+
+struct ATabEntry {
+    int xtyp;
+    int etyp;
+    int eref;
+    int low;
+    int high;
+    int elsz;
+    int size;
+};
+
+class SymbolTable {
+public:
+    std::vector<TabEntry> tab;
+    std::vector<BTabEntry> btab;
+    std::vector<ATabEntry> atab;
+
+    std::vector<int> display;
+    int current_level;
+
+    SymbolTable();
+
+    void initPredefined();
+
+    void enterBlock();
+    void leaveBlock();
+
+    int insertTab(const std::string& name, ObjClass obj, int type, int ref, int nrm, int adr);
+    int insertBTab();
+    int insertATab(int xtyp, int etyp, int eref, int low, int high, int elsz, int size);
+
+    int lookup(const std::string& name);
+    int lookupLocal(const std::string& name);
+};
+
+#endif
